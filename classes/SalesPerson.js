4a1cd1c8c7943a0b511be7ddbcb90d7fa18d71d2
Claire Robson <claire.robson1@barclays.com>
2024-10-02 14:34:22 +0100

    SalesPerson update

--- classes/SalesPerson.js
+++ classes/SalesPerson.js
@@ -1,7 +1,20 @@
-class SalesPerson {
+const Employee = require('./Employee');
 
+class SalesPerson extends Employee{
+    #totalSales;
+    constructor(name, title, clients = []){
+        super(name, title);
+        this.#totalSales = 0;
+        this.clients = clients; 
+    }
 
+    getSalesNumbers(){
+        return this.#totalSales;
+    }
 
+    makeSale(amount){
+        this.#totalSales += amount;
+    }
     
 }
 
