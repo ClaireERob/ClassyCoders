aa276f350e4f24879492491a1ead0965e3d1a6ac
Claire Robson <claire.robson1@barclays.com>
2024-10-02 14:21:40 +0100

    Manager update

--- classes/Manager.js
+++ classes/Manager.js
@@ -1,4 +1,20 @@
-class Manager {
+const Employee = require('./Employee');
+
+class Manager extends Employee {
+    #employeesManaged;
+    constructor(name, title, salary, department){
+    super(name, title, salary);
+    this.department = department;
+    this.#employeesManaged = [];
+}
+
+getEmployeesManaged(){
+    return this.#employeesManaged;
+}
+
+setEmployeesManaged(employee){
+    this.#employeesManaged.push(employee)
+}
 
 
 
